30fd84a9ccc9a1b1de3dba2abf365d7b5f91d800
mainak-cmd <76730011+mainak-cmd@users.noreply.github.com>
2024-01-10 16:48:34 +0530

    Update main.py
    ok

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def get_result(useid:str,testid:str):
     'Accuracy':0,
     'Speed':0,
     'ICA_Index':1,
-    'timestamp':datetime.now().strftime("%Y-%m-%d %H")
+    'timestamp':datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
     
     db[info_dict['Result']].insert_one(user_report)
